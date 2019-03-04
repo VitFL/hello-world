1e33261c0f1fd2bcdeb977b6f34b93103432c5f2
VitFL <vitfl@protonmail.com>
2019-03-04 15:07:44 +0200

    :bug: add check if user enter positive number; add default output if user enter number < 3

--- js-02-homework/js/index.js
+++ js-02-homework/js/index.js
@@ -1,6 +1,6 @@
 const n = parseInt(prompt('Enter Fibonacci number'));
-if (Number.isNaN(n)) {
-  alert('Input is not correct, click F5 and enter a number');
+if (Number.isNaN(n) || n <= 0) {
+  alert('Input is not correct, refresh a page and enter a positive number');
 } else {
   alert('Result of Loop Function: ' + fibonacciLoop(n) + '\nResult of Recursive Function: ' + fibonacciRecursion(n) + '\nResult of Array Function: ' + fibonacciArr(n) + '\nResult of Binet Function: ' + fibonacciBinet(n));
 }
@@ -9,7 +9,7 @@ function fibonacciLoop(num) {
   let first = 1;
   let second = 1;
   let next;
-
+  if (num < 3) return 1;
   for (let i = 3; i <= n; i++) {
     next = first + second;
     first = second;
@@ -19,7 +19,7 @@ function fibonacciLoop(num) {
 }
 
 function fibonacciRecursion(num) {
-  if (num < 2) return num;
+  if (num < 3) return 1;
   return fibonacciRecursion(num - 1) + fibonacciRecursion(num - 2);
 }
 
